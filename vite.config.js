5691988ed9adfa245e8335b35903ea08ade07bca
Jayniel Singh <jaynielsingh@yahoo.com>
2023-08-04 19:15:14 -0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
-  }
+
 });
